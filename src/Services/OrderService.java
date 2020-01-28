bd146878c1c29b0a5943e2c51073f10cec6c87e1
Rodrigo Jose Sarmento Oliveira <rodrigojsoliveira@gmail.com>
2020-01-28 20:02:53 -0300

    Update OrderService to rotate available pizza list

--- src/Services/OrderService.java
+++ src/Services/OrderService.java
@@ -22,29 +22,39 @@ public class OrderService {
 		List<Pizza> pizzasToOrder = new ArrayList<>();
 		List<Pizza> finalOrder = null;
 		int slicesOrdered = 0;
-		int maximumPossibleSlicesOrdered = 0;
+		int bestSliceOrder = 0;
+		int rotate = 0;
 
-		for (int x=orderParameters.getPizzasAvailable().size() - 1; x>1; x--) {
-
-			slicesOrdered = orderParameters.getPizzasAvailable().get(x).getSize();
-			pizzasToOrder.add(orderParameters.getPizzasAvailable().get(x));
-
-			for (int y = x - 1; y >= 0; y--) {
-
-				if (slicesOrdered + orderParameters.getPizzasAvailable().get(y).getSize() <= orderParameters
-						.getMaximumNumberOfSlicesToOrder()) {
-					slicesOrdered += orderParameters.getPizzasAvailable().get(y).getSize();
-					pizzasToOrder.add(orderParameters.getPizzasAvailable().get(y));
-				}
-			}
-
-			if (slicesOrdered > maximumPossibleSlicesOrdered) {
-				finalOrder = new ArrayList<>(pizzasToOrder);
-				maximumPossibleSlicesOrdered = slicesOrdered;
+		while (rotate <= orderParameters.getPizzasAvailable().size()) {
+			
+			if (bestSliceOrder == orderParameters.getMaximumNumberOfSlicesToOrder()) {
+				break;
 			}
 			
-			pizzasToOrder.clear();
-
+			for (int x=orderParameters.getPizzasAvailable().size() - 1; x > 0; x--) {
+	
+				slicesOrdered = orderParameters.getPizzasAvailable().get(x).getSize();
+				pizzasToOrder.add(orderParameters.getPizzasAvailable().get(x));
+	
+				for (int y = x - 1; y >= 0; y--) {
+	
+					if (slicesOrdered + orderParameters.getPizzasAvailable().get(y).getSize() <= orderParameters
+							.getMaximumNumberOfSlicesToOrder()) {
+						slicesOrdered += orderParameters.getPizzasAvailable().get(y).getSize();
+						pizzasToOrder.add(orderParameters.getPizzasAvailable().get(y));
+					}
+				}
+	
+				if (slicesOrdered > bestSliceOrder) {
+					finalOrder = new ArrayList<>(pizzasToOrder);
+					bestSliceOrder = slicesOrdered;
+				}
+				
+				pizzasToOrder.clear();
+			}
+			
+			Collections.rotate(orderParameters.getPizzasAvailable(), 1);
+			rotate += 1;
 		}
 		
 		Collections.reverse(finalOrder);
